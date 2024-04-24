85e5524d90401153cdcfe1152ac137ac15cce7a1
treadmilldestroyer <aaronarnold335@gmail.com>
2024-04-24 16:57:55 -0400

    First learning activity.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Console.WriteLine("Hello Prep1 World!");
+        // Console.WriteLine("This is my first line of C# code! Woohoo!!!");
+
+        // int number = 5;
+        // number = 8;
+        // number = number + 3;
+
+        // string color = "blue";
+        // Console.WriteLine(color);
+
+        // Console.Write("What is your favorite color?");
+        // string color = Console.ReadLine();
+        // Console.WriteLine($"Your favorite color is {color}.");
+        
+        Console.Write("What is your first name?");
+        string first_name = Console.ReadLine();
+
+        Console.Write("What is your last name?");
+        string last_name = Console.ReadLine();
+        
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
+
+
+
+
     }
 }
