86bc10d2ba95ac5c04be8b2b0352012c5ae5ea4c
treadmilldestroyer <aaronarnold335@gmail.com>
2024-04-24 17:07:56 -0400

    Camel case update.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -19,12 +19,12 @@ class Program
         // Console.WriteLine($"Your favorite color is {color}.");
         
         Console.Write("What is your first name?");
-        string first_name = Console.ReadLine();
+        string firstName = Console.ReadLine();
 
         Console.Write("What is your last name?");
-        string last_name = Console.ReadLine();
-        
-        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
 
 
 
