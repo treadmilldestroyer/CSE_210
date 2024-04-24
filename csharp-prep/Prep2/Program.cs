265ef1f72b465c012d23a7fccbd34aae6c0069b2
treadmilldestroyer <aaronarnold335@gmail.com>
2024-04-24 17:33:46 -0400

    Letter grade calculator added.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+
+        int gradePerc = int.Parse(grade);
+        string letter = ("");
+
+        if (gradePerc >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePerc >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePerc >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePerc >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Your final grade: {letter}");
+
+        if (gradePerc >= 70)
+        {
+            Console.Write("Congratulations on passing the class! Great Job! I am proud of you!");
+        }
+        else
+        {
+            Console.Write("Do not give up! You now have so much knowledge in this class, when you retake the class, you're going to crush it!");
+        }
+
     }
 }
