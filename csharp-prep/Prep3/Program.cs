5bd8483e3fbbca85e8b19bf6e679d155bbda4075
treadmilldestroyer <aaronarnold335@gmail.com>
2024-05-02 18:20:18 -0400

    Finished Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int answerNumber = randomGenerator.Next(1, 100);
+        
+        // Console.WriteLine("What is the magic number? ");
+        // string magicNumber = Console.ReadLine();
+        // int answerNumber = int.Parse(magicNumber);
+
+        Console.WriteLine("What is your guess? ");
+        string userGuess = Console.ReadLine();
+        int guess = int.Parse(userGuess);
+        int count = 1;
+
+        while (guess != answerNumber)
+            if (guess < answerNumber)
+            {
+                Console.WriteLine("Higher");
+                guess = int.Parse(Console.ReadLine());
+                count += 1;
+            }
+            else
+            {
+                Console.WriteLine("Lower");
+                guess = int.Parse(Console.ReadLine());
+                count += 1;
+            }
+        if (guess == answerNumber)
+        {
+            Console.WriteLine("You got it!");
+            Console.WriteLine($"It took you {count} guesses.");
+        }
+
+
+
+
     }
 }
