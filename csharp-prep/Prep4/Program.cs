c310a5186a88b4d33050c8c7061e56f88f635d16
treadmilldestroyer <aaronarnold335@gmail.com>
2024-05-02 19:19:17 -0400

    Finished prep 4.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> listNumbers = new List<int>();
+        int number = -1;
+
+        while (number != 0)
+        {
+            Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+            string numbers = Console.ReadLine();
+            number = int.Parse(numbers);
+            if (number != 0)
+            {
+                listNumbers.Add(number);
+            }
+        }    
+
+        
+        int sum = 0;
+        int greatest = 0;
+        foreach (int value in listNumbers)
+        {
+            sum += value;
+            if (value > greatest)
+            {
+                greatest = value;
+            }
+        }
+        Console.WriteLine($"The sum is: {sum}");
+        int average = sum / listNumbers.Count;
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The greatest number is: {greatest}");
+
+       
+
+
+
     }
 }
