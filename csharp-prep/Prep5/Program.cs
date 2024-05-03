28e08528043eaa97de2fccd895119384c48930ea
treadmilldestroyer <aaronarnold335@gmail.com>
2024-05-02 20:17:12 -0400

    Finished prep 5.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+
+        string name = PromptUserName();
+
+        int number = PromptUserNumber();
+        
+        int squaredNumber = SquareNumber(number);
+        
+        DisplayResult(name, squaredNumber);
+    }
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.WriteLine("What is your name? ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+    static int PromptUserNumber()
+    {
+        Console.WriteLine("What is your favorite number? ");
+        int number = int.Parse(Console.ReadLine());
+
+        return number;
+    }
+    static int SquareNumber(int number)
+    {
+        int newNumber = number * number;
+
+        return newNumber;
+    }
+    static void DisplayResult(string name, int newNumber)
+    {
+        Console.WriteLine($"Name: {name}");
+        Console.WriteLine($"Your favorite number squared is: {newNumber}");
     }
 }
