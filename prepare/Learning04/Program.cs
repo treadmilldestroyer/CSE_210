0d965da44b191d077e2ffce263c03df5e4aa92d1
treadmilldestroyer <aaronarnold335@gmail.com>
2024-06-06 19:50:25 -0400

    Prepare 4

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,19 @@
 using System;
+using System.ComponentModel;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment1 = new Assignment("Aaron", "Counting");
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment math1 = new MathAssignment("Aaron", "Math", "Section 1", "#1-5");
+        Console.WriteLine(assignment1.GetSummary());
+        Console.WriteLine(math1.GetHomeworkList());
+
+        WritingAssignment write1 = new WritingAssignment("Aaron", "English", "Title of Book");
+        Console.WriteLine(assignment1.GetSummary());
+        Console.WriteLine(write1.GetWritingInformation());    
     }
 }
---
+++ prepare/Learning04/WritingAssignment.cs
@@ -0,0 +1,15 @@
+using System;
+public class WritingAssignment : Assignment
+{
+    public string _title;
+
+    public WritingAssignment(string studentName, string topic, string title) : base(studentName, topic)
+    {
+        _title = title;
+    }
+
+    public string GetWritingInformation()
+    {
+        return $"\"{_title}\" by {_studentName}";
+    }
+}
